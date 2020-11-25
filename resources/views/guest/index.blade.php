1b618a4f59324cd79423aca03faf9d9180aef96a
Elisa <elisa.torazza@gmail.com>
2020-11-25 13:09:25 +0100

    Img_index

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -5,16 +5,23 @@
 @endsection
 
 @section('mainContent')
-    <body>
-        <div class="card-group">
-            {{-- @foreach ($properties as $property)
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('$property->img')}}" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$propertty->title}}</h5>
-                    </div>
-                </div>
-            @endforeach --}}
-        </div>
-    </body>
+<section class="home_guest">
+    <div class="container">
+                
+            <ul class="flat_list">
+                @foreach ($properties as $property)
+                    <li>
+                        <div class="img-responsive">
+                            <img src="{{$property->flat_image}}" alt="Home Picture">
+                        </div>
+                        <div class="overlay">
+                        <div class="small-container">
+                        <a href="/properties/{{$property->id}}">{{$property->title}}</a>
+                        </div>
+                        </div>
+                    </li>
+                @endforeach     
+            </ul>
+    </div>
+</section>
 @endsection
