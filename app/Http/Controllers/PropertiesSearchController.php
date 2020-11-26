094aad7315da158d9756ac1ea42e44c55580c5f5
Jacopo <jakonigro@gmail.com>
2020-11-26 15:11:11 +0100

    wip search

--- app/Http/Controllers/PropertiesSearchController.php
+++ app/Http/Controllers/PropertiesSearchController.php
@@ -7,11 +7,22 @@ use App\Property;
 
 class PropertiesSearchController extends Controller
 {
-    //metodo index
-    public function search()
+    //metodo getProperties per ottenere un json con le proprietà
+
+    public function getProperties()
     {
+
       $properties = Property::all();
+      
       return response()->json(compact("properties"));
+    }
+      //metodo index per mostrare pagina di ricerca
+
+    public function index()
+    {
+      return view("guest.search");
+
+
     }
 
 }
