0330cf9cb8f1e4443b3278f6a57cf4adb17f9224
Jessica Donzelli <jedonzelli@gmail.com>
2020-11-27 19:17:00 +0100

    Updated guest show view

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -8,75 +8,108 @@
 <section class="detail">
     <div class="wrapper">
 
+        {{-- DETTGLI STRUTTURA --}}
         <div class="property-details">
-            <div class="img-container">  
-                <img class="img-show" src="{{$property->flat_image}}" alt="">
-            </div> 
-            <h1>{{$property->title}}</h1>
-            <p>{{$property->description}}</p>  
-            <div class="service_description">
-                <h5>Locali: {{$property->rooms_number}}</h5>
-                <h5>Letti: {{$property->beds_number}}</h5>
-                <h5>Bagni: {{$property->bathrooms_number}}</h5>
-                <h5>Metri Quadrati: {{$property->square_meters}}</h5>
 
-                @if(!$result->isEmpty())
-                {{-- // $result is not empty --}}
-           @else
+            {{-- IMMAGINE COPERTINA E TITOLO --}}
+            <div class="title-wrapper"> 
+                <div class="img-container"> 
+                    <img class="img-show" src="{{$property->flat_image}}" alt="">
+                </div>
+                <h1>{{$property->title}}</h1>
+                
+                <!-- BOTTONE -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Invia un messaggio alla struttura
+                </button>
+                <!-- //BOTTONE -->
+  
+                <!-- MODALE FORM MESSAGGIO -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Form di Contatto</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>
+                            <!-- FORM -->
+                            <div class="modal-body form-container">
+                                <form action="action_page.php">
+                                    <label for="fname">Nome</label>
+                                    <input type="text" id="fname" name="firstname" placeholder="Nome">
+                    
+                                    <label for="lname">Cognome</label>
+                                    <input type="text" id="lname" name="lastname" placeholder="Cognome">
             
+                                    <label for="email">Email</label>
+                                    <input type="text" id="email" name="email" placeholder="Indirizzo Email">
+                                    {{-- @if (Auth::check()) value="{{$email}}" @endif --}}
         
-                <h5>Extra:</h5>
-                    @foreach ($property->extras as $extra)
-                        <p>{{$extra->name}}</p>
-                    @endforeach
-                @endif
-            </div>      
-        </div>
+                                    <label for="subject">Messaggio</label>
+                                    <textarea id="subject" name="subject" placeholder="Scrivi qualcosa..." style="height:200px"></textarea> 
+                                </form>
+                            </div>
+                            <!-- //FORM -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Invia</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <!-- //MODALE FORM MESSAGGIO -->
+            
+            </div>
+            {{-- //IMMAGINE COPERTINA E TITOLO --}}
 
+            {{-- DESCRIZIONE --}}
+            <div class="description">
+                <div>
+                <h6><i class="fas fa-door-open"></i> {{$property->description}}</h6>  
+                </div>
+            </div>
+            {{-- //DESCRIZIONE --}}
+        
+            {{-- LOCALI --}}
+            <div class="features">
+                <div>
+                    <h5>Caratteristiche</h5>
+                    <h6><i class="fas fa-home"></i> Locali: {{$property->rooms_number}}</h6>
+                    <h6><i class="fas fa-bed"></i> Letti: {{$property->beds_number}}</h6>
+                    <h6><i class="fas fa-shower"></i> Bagni: {{$property->bathrooms_number}}</h6>
+                    <h6><i class="fas fa-th"></i> Metri Quadri: {{$property->square_meters}}</h6>
+                </div> 
+            </div>
+            {{-- //LOCALI --}}
+
+            {{-- EXTRA --}}
+            <div class="extra">
+                <div>
+                    @if($property->extras->isEmpty())
+                    <p>non ci sono extra</p>
+                    @else
+                        <h5>Extra</h5>
+                            @foreach ($property->extras as $extra)
+                                <p><i class="far fa-star"></i> {{$extra->name}}</p>
+                            @endforeach
+                    @endif
+                </div>
+            </div>
+            {{-- //EXTRA --}}    
+
+        </div>
+        
+        {{-- MAPPA --}}
         <div class="map-container">
-            <h3>Indirizzo e geolocalizzazione:</h3>
-                <img src="{{asset('img/maps.png')}}" alt="maps">
-                <p>Viale Italia 78, 78000 Pistoia</p>
+            <h2>Indirizzo e geolocalizzazione</h2>
+            <h5><i class="fas fa-map-marker-alt"></i> Viale Italia 78, 78000 Pistoia</h5>
+                <div class="map"></div>
                 {{-- <h4>{{$property->address}}</h4> --}}
         </div>
-
-        <div class="message-container">
-        
-            <button onclick="document.getElementById('id01').style.display='block'" class="msg-button">Invia un messaggio al proprietario</button>
-          
-            <div id="id01" class="w3-modal">
-              <div class="w3-modal-content w3-card-4">
-                <div class="w3-container w3-teal"> 
-                  <span onclick="document.getElementById('id01').style.display='none'" 
-                  class="w3-button w3-display-topright">&times;</span>
-                </div>
-
-                <div class="form-container">
-                    <form action="action_page.php">
-                        <label for="fname">Nome</label>
-                        <input type="text" id="fname" name="firstname" placeholder="Your name..">
-        
-                        <label for="lname">Cognome</label>
-                        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-        
-                        <label for="email">Email</label>
-                        <input type="text" id="email" name="email" placeholder="Your email adress..">   
-                        {{-- se utente e' loggato email viene gia' inserita --}}
-        
-                        <label for="subject">Messaggio</label>
-                        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea> 
-        
-                        <input type="submit" value="Submit">
-                    </form>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        
+        {{-- MAPPA --}}
 
     </div>
+    {{-- //DETTGLI STRUTTURA --}}
 </section>
 @endsection
-
-
