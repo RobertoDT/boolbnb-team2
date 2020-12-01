1117ca6c04f61ea86fba014fffe0125228220f73
Elisa <elisa.torazza@gmail.com>
2020-12-01 20:49:39 +0100

    index in evidenza-extra_search

--- resources/views/layouts/nav_bar.blade.php
+++ resources/views/layouts/nav_bar.blade.php
@@ -19,7 +19,12 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                
+                {{-- Icona filtro --}}
+                <div class="filter">
+                    <i class="fas fa-filter funnel"></i>
+                </div>
+                {{-- /Icona filtro --}}
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -45,6 +50,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{route('admin.properties.index')}}">I miei appartamenti<i class="fas fa-home"></i></a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
@@ -61,5 +67,22 @@
                 </div>
             </div>
         </nav>
+        <div class="filter_container d-none">
+            {{-- Prendo gli extra, li ciclo e li inserisco nella parte bassa dell'header --}}
+            @foreach ($extras as $extra)
+            <div class="small_filter_container">
+                <label for="{{$extra->id}}"> {{$extra->name}}</label>
+                <input type="checkbox" name="extras[]" id="{{$extra->id}}" value="{{$extra->id}}" {{ (is_array(old('extras')) and in_array($extra_id, old('extras'))) ? ' checked' : '' }}>
+            </div>
+            {{-- Prendo gli extra, li ciclo e li inserisco nella parte bassa dell'header --}}
+            @endforeach
+            {{-- Barra per selezionare la distanza dalla search--}}
+            <div class="bar_tab width_30">
+                <div class="bar_up width_25">
+                  <div class="bar_up overlay_green"></div>
+                </div>
+              </div>
+            {{-- barra per selezionare la distanza dalla search--}}
+          </div>
     </div>
 </header>
