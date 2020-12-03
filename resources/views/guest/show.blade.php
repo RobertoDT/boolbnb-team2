74dc31c26f03e8d20bb346843a9fe49c352b5f7c
Jacopo <jakonigro@gmail.com>
2020-12-03 18:23:55 +0100

    show.guest

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -7,122 +7,78 @@
 @include('layouts.header-general')
 
 @section('mainContent')
-<section class="detail">
-    <div class="large-wrapper">
-        <div class="wrapper">
+<section class="main-content">
+    <div class="wrapper">
 
-            {{-- DETTGLI STRUTTURA --}}
-            <div class="property-details">
+        {{-- APARTMENT IMAGE --}}
+        <div class="img-container">
+            <img class="img-show" src="{{$property->flat_image}}" alt="Foto appartamento">
+            <div class="address">
+                <p>ROMA</p>
+            </div> 
+        </div>
+        {{-- /APARTMENT IMAGE --}}
 
-                {{-- IMMAGINE COPERTINA E TITOLO --}}
-                <div class="title-wrapper">
-                    <div class="img-container">
-                        <img class="img-show" src="{{$property->flat_image}}" alt="Foto appartamento">
-                    </div>
-                    <h1>{{$property->title}}</h1>
+        <h1>{{$property->title}}</h1>
 
-                    <!-- BOTTONE -->
-                    <button type="button" class="btn btn-primary modifing_link
-                    " data-toggle="modal" data-target="#exampleModal">
-                        Invia un messaggio alla struttura
-                    </button>
-                    <!-- //BOTTONE -->
+        {{-- INFORMATIONS AND MAP --}}
+        <div class="info-map">
 
-                    <!-- MODALE FORM MESSAGGIO -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Form di Contatto</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>
-                                <!-- FORM -->
+            {{-- MAP  --}}
+            <div class="container-map">
+                <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
+        
+                <div id="map-example-container-paris"></div>
+                {{-- <input type="hidden" id="input-map-paris"/> --}}
+                <input type="hidden" id="input-map-paris">
+                <style>.algolia-places {display: none}</style>
+                {{-- prendiamo coordinate da $property --}}
+                <input type="hidden" id="latitude" value="{{$property->latitude}}">
+                <input type="hidden" id="longitude" value="{{$property->longitude}}">
+            </div>
+            {{-- /MAP  --}}
 
-                                <div class="modal-body form-container">
-                                    <form action="{{route("messages.store", $property->id)}}" method="POST">
-
-                                      @csrf
-                                      @method("POST")
-
-                                        <label for="email">Email</label>
-                                        <input class="form-control" type="email" id="email" name="email" placeholder="Indirizzo Email" required @if (Auth::check()) value="{{$email}}" @endif>
-
-
-                                        <label for="text">Messaggio</label>
-                                        <textarea class="form-control" id="text" name="text" placeholder="Scrivi messaggio..." style="height:200px" required>{{old("text") ?? old("text")}}</textarea>
-
-                                        <div class="modal-footer">
-                                          <button type="submit" class="btn btn-primary">Invia</button>
-                                        </div>
-                                    </form>
-                                </div>
-                                <!-- //FORM -->
-                        </div>
-                        </div>
-                    </div>
-                    <!-- //MODALE FORM MESSAGGIO -->
-
-                </div>
-                {{-- //IMMAGINE COPERTINA E TITOLO --}}
-
-                {{-- DESCRIZIONE --}}
+            {{-- DESCRIPTION --}}
+            <div class="container-info">
                 <div class="description">
-                    <div>
-                    <h6><i class="fas fa-door-open"></i> {{$property->description}}</h6>
-                    </div>
+                    <h4>DESCRIPTION</h4>
+                    <h6><i class="fas fa-door-open gradient"></i> {{$property->description}}</h6>
                 </div>
-                {{-- //DESCRIZIONE --}}
+            {{-- /DESCRIPTION --}}
+            {{-- FEATURE-EXTRAS    --}}
+            
+                <div class="features-extras">
+                    <div class="features">
+                        <h4>FEATURES</h4>
 
-                {{-- LOCALI --}}
-                <div class="features">
-                    <div>
-                        <h5>Caratteristiche</h5>
-                        <h6><i class="fas fa-home"></i> Locali: {{$property->rooms_number}}</h6>
-                        <h6><i class="fas fa-bed"></i> Letti: {{$property->beds_number}}</h6>
-                        <h6><i class="fas fa-shower"></i> Bagni: {{$property->bathrooms_number}}</h6>
-                        <h6><i class="fas fa-th"></i> Metri Quadri: {{$property->square_meters}}</h6>
+                        {{-- FEATURES --}}
+                        <h6><i class="fas fa-home gradient"></i> Locali: {{$property->rooms_number}}</h6>
+                        <h6><i class="fas fa-bed gradient"></i> Letti: {{$property->beds_number}}</h6>
+                        <h6><i class="fas fa-shower gradient"></i> Bagni: {{$property->bathrooms_number}}</h6>
+                        <h6><i class="fas fa-th gradient"></i> Metri Quadri: {{$property->square_meters}}</h6>
+                        {{-- /FEATURES --}}
                     </div>
-                </div>
-                {{-- //LOCALI --}}
 
-                {{-- EXTRA --}}
-                <div class="extra">
-                    <div>
+                    {{-- EXTRAS --}}
+                    <div class="extras">
                         @if($property->extras->isEmpty())
-                        <p>non ci sono extra</p>
+                        <p>Non ci sono extra</p>
                         @else
-                            <h5>Extra</h5>
+                            <h4>EXTRAS</h4>
                                 @foreach ($property->extras as $extra)
-                                    <p><i class="far fa-star"></i> {{$extra->name}}</p>
+                                    <p><i class="far fa-star gradient"></i> {{$extra->name}}</p>
                                 @endforeach
                         @endif
                     </div>
+                    {{-- /EXTRAS --}}
+                    {{-- /FEATURE-EXTRAS --}}
                 </div>
-                {{-- //EXTRA --}}
-
             </div>
-            {{-- //DETTGLI STRUTTURA --}}
 
-        </div>
-    </div>
-
-        <div class="wrapper">
-            {{-- MAPPA --}}
-            <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
-    
-            <div id="map-example-container-paris"></div>
-            <input type="hidden" id="input-map-paris" class="form-control" placeholder="Find a street in Paris, France. Try &quot;Rivoli&quot;" />
+        </div>    
+        {{-- /INFORMATIONS AND MAP --}}   
+                
         
-            {{-- prendiamo coordinate da $property --}}
-            <input type="hidden" id="latitude" value="{{$property->latitude}}">
-            <input type="hidden" id="longitude" value="{{$property->longitude}}">
-                {{-- MAPPA --}}
-            </div>
-            {{-- MAPPA --}}
-        </div>
-
-
+    </div>
 </section>
 @endsection
