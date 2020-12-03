4786e8e7b0c200fb0459c6fbfe4f9a60930b40ed
Jessica Donzelli <jedonzelli@gmail.com>
2020-12-03 19:24:38 +0100

    aggiunte a index admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 require('./bootstrap');
+require("././partials/admin.index.js");
 require("././partials/search.js");
 require("././partials/guest.index.js");
 require("././partials/guest.show.js");
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,7 +24,8 @@
 
     @include('layouts.footer')
     
+  
   <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
-
+  <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
