c08a30dc3e3dd092d3b9c1e58f8a1950a277950f
Elisa <elisa.torazza@gmail.com>
2020-12-04 08:47:39 +0100

    Esteso modifiche altri header

--- resources/views/layouts/header-general.blade.php
+++ resources/views/layouts/header-general.blade.php
@@ -2,39 +2,37 @@
   <div class="down fixed_search">
     <nav class="navbar navbar-expand-md navbar-light">
         <div class="container">
-            <div class="row">
+                {{-- logo immagine --}}
                 <div class="logo_img">
                     <a class="navbar-brand" href="{{ url('/') }}">
                         <img src="{{asset('img/cover.png')}}" alt="Logo Boolbnb">
                     </a>
                 </div>
-            </div>
-            <div class="row width_50">
+                {{-- /logo immagine --}}
+
                {{-- INPUT SEARCH --}}             
-               <div class="start_search">                 
+               <div class="start_search order">                 
                     <form class="form-inline my-2 my-lg-0" action="{{route("search")}}" method="GET">                     
                         @method('GET')                 
-                        <div class="start_search form-inline my-2 my-lg-0">                     
+                        <div class="div_search form-inline my-2 my-lg-0">                     
                             <input class="form-control mr-sm-2 input_search" type="search" id="address" name="search" placeholder="Where are we going?" />                 
                         </div>                 
-                        <button class="btn btn-dark my-2 my-sm-0 modifing_link"  class="search_button" type="submit">Search</button>
+                        <button class="btn btn-dark my-2 my-sm-0 modifing_link search_write"  class="search_button" type="submit">Search</button>
                     </form>
                 </div>
-            </div>
-        <div class="row">
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <button class="navbar-toggler bars" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse clearfix">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
 
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
+                <ul class="navbar-nav ml-auto float-right text-right">
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
@@ -46,12 +44,12 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown width_100">
+                        <li class="nav-item dropdown width_100 m_0 p_5">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->firstname }} {{Auth::user()->lastname}}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <div class="dropdown-menu dropdown-menu-right text-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{route('admin.properties.index')}}">I miei appartamenti <i class="fas fa-home"></i></a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
@@ -67,11 +65,50 @@
                     @endguest
                 </ul>
             </div>
-        </div>
+            {{-- nostro menu dropdown --}}
+            <div class="collapse hamburger clearfix" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto float-right text-right">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item">
+                                
+                                <a class="dropdown-item" href="{{route('admin.properties.index')}}">My apartments</a>
+                            </li>
+                        <li class="nav-item">
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                            </li>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+            {{-- nostro menu dropdown --}}
         </div>
     </nav>
-
     {{-- <main class="py-4">
         @yield('content')
     </main> --}}
--- resources/views/layouts/header-index.blade.php
+++ resources/views/layouts/header-index.blade.php
@@ -6,7 +6,6 @@
                     <div class="logo_img">
                         <a class="navbar-brand" href="{{ url('/') }}">
                             <img src="{{asset('img/cover.png')}}" clas="logo_regular" alt="Logo Boolbnb">
-                            {{-- <img src="{{asset('img/logo_min.png')}}" class="logo_min" alt="Logo Boolbnb"> --}}
                         </a>
                     </div>
                     {{-- /Div logo --}}
@@ -15,20 +14,19 @@
                 <div class="start_search order">                 
                     <form class="form-inline my-2 my-lg-0" action="{{route("search")}}" method="GET">                     
                         @method('GET')                 
-                        <div class="start_search form-inline my-2 my-lg-0">                     
+                        <div class="div_search form-inline my-2 my-lg-0">                     
                             <input class="form-control mr-sm-2 input_search" type="search" id="address" name="search" placeholder="Where are we going?" />                 
                         </div>                 
                         <button class="btn btn-dark my-2 my-sm-0 modifing_link search_write"  class="search_button" type="submit">Search</button>
-                        <button class="btn btn-dark my-2 my-sm-0 modifing_link search_icon"  class="search_button" type="submit"><i class="fas fa-search"></i></button>
                     </form>
                 </div>
 
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            {{-- bottone hamburger --}}
+            <button class="navbar-toggler bars" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse clearfix">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
 
@@ -47,14 +45,13 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown width_100">
+                        <li class="nav-item dropdown width_100 m_0 p_5">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->firstname }} {{Auth::user()->lastname}}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right text-right bg_transparent" aria-labelledby="navbarDropdown">
-                                {{-- <p class="search_responsive">Search</p> --}}
-                                <a class="dropdown-item" href="{{route('admin.properties.index')}}">I miei appartamenti <i class="fas fa-home"></i></a>
+                            <div class="dropdown-menu dropdown-menu-right text-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{route('admin.properties.index')}}">My apartments</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
@@ -70,12 +67,53 @@
                 </ul>
             </div>
 
+            {{-- nostro menu dropdown --}}
+                <div class="collapse hamburger clearfix" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto float-right text-right">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item">
+                                    
+                                    <a class="dropdown-item" href="{{route('admin.properties.index')}}">My apartments</a>
+                                </li>
+                            <li class="nav-item">
+
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                </li>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            {{-- nostro menu dropdown --}}
+
+
         </div>
     </nav>
 
-    {{-- <main class="py-4">
-        @yield('content')
-    </main> --}}
 </div>
 
 </header>
