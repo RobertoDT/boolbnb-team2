8fc96ff1be676ad22f244bd7fc3e5affebf43fee
Jessica Donzelli <jedonzelli@gmail.com>
2020-12-04 19:05:25 +0100

    admin show

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
+require("././partials/admin.show.js");
 require("././partials/admin.index.js");
 require("././partials/search.js");
 require("././partials/guest.index.js");
 require("././partials/guest.show.js");
-require("././partials/admin.show.js");
+
