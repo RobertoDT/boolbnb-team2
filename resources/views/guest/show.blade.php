ca392ae55a10a13e1983aec0a476313b069148d4
Jacopo <jakonigro@gmail.com>
2020-12-06 18:31:09 +0100

    wip search.guestr

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -21,46 +21,41 @@
             {{-- /APARTMENT IMAGE --}}
 
             <div class="send-message">
-                <h3>Send a message for more informations </h3><i class="fas fa-arrow-right fa-2x"></i><i class="far fa-envelope fa-2x"></i>
+                <h3>Send a message for more informations </h3><i class="fas fa-arrow-right fa-2x"></i><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap"><i class="far fa-envelope fa-2x"></i></button>
             </div>
+           
 
             {{-- MODALE --}}
+            
+
             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+            <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Form di Contatto</h5>
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+                    <span aria-hidden="true">&times;</span>
                     </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Recipient:</label>
+                        <input type="text" class="form-control" id="recipient-name">
                     </div>
-                        <!-- FORM -->
-
-                        <div class="modal-body form-container">
-                            <form action="{{route("messages.store", $property->id)}}" method="POST">
-
-                              @csrf
-                              @method("POST")
-
-                                <label for="email">Email</label>
-                                <input class="form-control" type="email" id="email" name="email" placeholder="Indirizzo Email" required @if (Auth::check()) value="{{$email}}" @endif>
-
-
-                                <label for="text">Messaggio</label>
-                                <textarea class="form-control" id="text" name="text" placeholder="Scrivi messaggio..." style="height:200px" required>{{old("text") ?? old("text")}}</textarea>
-
-                                <div class="modal-footer">
-                                  <button type="submit" class="btn btn-primary">Invia</button>
-                                </div>
-                            </form>
-                        </div>
-                        <!-- //FORM -->
+                    <div class="form-group">
+                        <label for="message-text" class="col-form-label">Message:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Send message</button>
                 </div>
                 </div>
             </div>
-            <!-- //MODALE FORM MESSAGGIO -->
-
-                
+            </div>
         <!-- //MODALE FORM MESSAGGIO -->
 
         {{-- INFORMATIONS AND MAP --}}
