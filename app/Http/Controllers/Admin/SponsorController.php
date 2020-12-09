ce88af08c17e981db1977fec85249881b9a7e22b
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-12-09 19:03:29 +0100

    Add payments, statistics, with style

--- app/Http/Controllers/Admin/SponsorController.php
+++ app/Http/Controllers/Admin/SponsorController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 //importo controller di base
 use App\Http\Controllers\Controller;
+//autenticazione
+use Illuminate\Support\Facades\Auth;
 // BRAINTREE
 use Braintree;
+//models
+use App\Sponsor;
+use App\Property;
+//carbon
+use Carbon\Carbon;
+//Facades DB
+use Illuminate\Support\Facades\DB;
 
 class SponsorController extends Controller
 {
@@ -26,15 +35,20 @@ class SponsorController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    {   
+    {
         $gateway = new Braintree\Gateway([
             'environment' => config('services.braintree.environment'),
             'merchantId' => config('services.braintree.merchantId'),
             'publicKey' => config('services.braintree.publicKey'),
             'privateKey' => config('services.braintree.privateKey')
         ]);
+        $sponsors = Sponsor::all();
+
+        $user_id = Auth::id();
+        $properties = Property::where("user_id", $user_id)->get();
+
         $token = $gateway->ClientToken()->generate();
-        return view("admin.properties.sponsors", compact('token'));
+        return view("admin.properties.sponsors", compact('token', 'sponsors', 'properties'));
     }
 
     /**
@@ -45,6 +59,24 @@ class SponsorController extends Controller
      */
     public function store(Request $request)
     {
+        // validation
+        $request->validate([
+          "sponsor_id" => "required",
+          "property_id" => "required"
+        ]);
+
+        $now = Carbon::now();
+
+        $id_sponsored = DB::table('property_sponsor')
+        ->where('end_sponsor', '>=', $now)
+        ->where('property_id', '=', $request->property_id)
+        ->first();
+
+        if($id_sponsored != null){
+            $end_sponsor = $id_sponsored->end_sponsor;
+            return back()->with('sponsor_active_message', 'La tua sponsorizzazione è ancora valida fino al '.$end_sponsor.' termina questa per effettuarne una nuova. Grazie il Team BoolB&B');
+        }
+
         $gateway = new Braintree\Gateway([
             'environment' => config('services.braintree.environment'),
             'merchantId' => config('services.braintree.merchantId'),
@@ -54,6 +86,12 @@ class SponsorController extends Controller
 
         $amount = $request->amount;
         $nonce = $request->payment_method_nonce;
+        $property = Property::find($request->property_id);
+        $sponsor = Sponsor::find($request->sponsor_id);
+        $property = $request->property_id;
+
+        $end = $now->addHours($sponsor->duration)->format('Y-m-d H:i:s');
+        $created_at = Carbon::now();
 
         $result = $gateway->transaction()->sale([
             'amount' => $amount,
@@ -62,13 +100,17 @@ class SponsorController extends Controller
                 'submitForSettlement' => true
             ]
         ]);
-        
+
         if ($result->success) {
             $transaction = $result->transaction;
             // inserire store sponsors
-            return back()->with('success_message', 'Payment went through, Thak you Mate!');
-            // return view('admin.properties.index')->with('success_message', 'Payment went true, Thak you Mate!');
-            
+            $sponsor->properties()->attach(
+              $property,
+              ['created_at' => $created_at, 'end_sponsor' => $end]
+            );
+            // return back()->with('success_message', 'Payment went through, Thak you Mate!');
+            return redirect()->route('admin.properties.show', $property)->with('success_message', 'Payment went true, Thak you Mate!');
+
         } else {
             $errorString = "";
 
